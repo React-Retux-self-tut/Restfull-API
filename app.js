2d2319ca851c10254298895d1f5094bf06cc212e
haluskua <haluskua.one@gmail.com>
2020-05-24 20:00:31 +1200

    added DB_USER with parced password and user name in .env file

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv/config');
 
 const app = express();
 
-
-
 //middlewares in express
-    //---- functions that execute
+//---- functions that execute
 
-    app.use('/post', () => {
-        console.log('This is a middleware running')
-    });
+app.use("/post", () => {
+  console.log("This is a middleware running");
+});
 
 //ROUTES
 
-app.get('/', (req, res) => {
-    res.send(
-        "We are on home - page"
-    );
+app.get("/", (req, res) => {
+  res.send("We are on home - page");
 });
-app.get('/post', (req, res) => {
-    res.send(
-        "We are on home - post"
-    );
+app.get("/post", (req, res) => {
+  res.send("We are on home - post");
 });
+
+//connect to DB
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.DB_CONNECTION;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true  });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  //CHECK if datatbase is connected to server
+  console.log('connected to DB!')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 //HOW DO WE START LISTENNING TO THE SERVER
-
 app.listen(4000);
+
+
